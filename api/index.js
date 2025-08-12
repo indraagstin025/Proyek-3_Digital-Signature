e06c39ca006c6c5a05375319737073e90eeef0b9
indraagstin025 <indraagustin012@gmail.com>
2025-08-12 21:17:19 +0700

    push

--- api/index.js
+++
@@ -1,1 +0,0 @@
-import dotenv from 'dotenv';import path from 'path';dotenv.config({ path: path.resolve(process.cwd(), '.env') });console.log('SUPABASE_URL =', process.env.SUPABASE_URL);import app from '../src/app.js';export default app;
--- server.js
+++
@@ -1,1 +0,0 @@
-import app from './src/app.js';const port = process.env.PORT || 3000;app.listen(port, () => {    console.log(`✅ Server berjalan di http://localhost:${port}`);});
