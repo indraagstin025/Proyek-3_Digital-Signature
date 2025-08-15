069d2888e8a7f3834b1046aad2905acd34122670
indraagstin025 <indraagustin012@gmail.com>
2025-08-15 23:38:27 +0700

    edit Supabase Client

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
+import 'dotenv/config.js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
-
 export const supabaseBucket = process.env.SUPABASE_BUCKET_NAME;
+export const SITE_URL = process.env.SITE_URL;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
