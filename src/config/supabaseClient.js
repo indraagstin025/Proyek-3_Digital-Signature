057370badd3b30f2ec8499ff10de0ba4efbe599c
indraagstin025 <indraagustin012@gmail.com>
2025-08-15 22:45:33 +0700

    add

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
+
+export const supabaseBucket = process.env.SUPABASE_BUCKET_NAME;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
